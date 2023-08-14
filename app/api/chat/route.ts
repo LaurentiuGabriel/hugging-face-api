90d94b0467e5ba8f4ace2d8996f7ac6fde29bba8
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2023-08-14 20:58:02 +0200

    initial commit

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -13,7 +13,7 @@ export async function POST(req: Request) {
   const { messages } = await req.json()
 
   const response = Hf.textGenerationStream({
-    model: 'meta-llama/Llama-2-70b-chat-hf',
+    model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
     inputs: experimental_buildOpenAssistantPrompt(messages),
     parameters: {
       max_new_tokens: 200,
