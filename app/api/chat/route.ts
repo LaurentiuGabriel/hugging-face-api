534df9c62ef423f9081e54a3fbd5cff23fc9fa47
LaurentiuGabriel <laurentiu.raducu@gmail.com>
2023-08-14 21:24:44 +0200

    adding prompt engineering

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -8,13 +8,39 @@ const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY)
 // IMPORTANT! Set the runtime to edge
 export const runtime = 'edge'
 
+function buildPrompt(
+  messages: { content: string; role: 'system' | 'user' | 'assistant' }[]
+) {
+  return (
+    messages
+      .map(({ content, role }) => {
+        if (role === 'user') {
+          return `<|prompter|>${content}<|endoftext|>`
+        } else {
+          return `<|assistant|>${content}<|endoftext|>`
+        }
+      })
+      .join('') + '<|assistant|>'
+  )
+}
+
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json()
+  let { messages } = await req.json()
+
+  const promptEngineering = "Act as a top social media manager. You are an expert on growing the social media accounts of people. If someone asks you something totally unrelated to social media, you will respond with a generic text saying you are only trained to respond to social media-related queries. But if it touches social media, then you should respond. The message is this: "
+
+  messages = messages.map((message: { content: string; role: 'system' | 'user' | 'assistant' }) => {
+    if (message.role === 'user') {
+      return { ...message, content: `${promptEngineering} ${message.content}` };
+    } else {
+      return message;
+    }
+  });
 
   const response = Hf.textGenerationStream({
     model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
-    inputs: experimental_buildOpenAssistantPrompt(messages),
+    inputs: buildPrompt(messages),
     parameters: {
       max_new_tokens: 200,
       // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
